7d4f0e8f35d8486ff6f71401a17499964169a993
DennyNeves <168123700+DennyNeves@users.noreply.github.com>
2024-12-20 10:16:55 -0300

    Adicionei som!

--- main.ts
+++ main.ts
@@ -3,4 +3,5 @@ basic.forever(function () {
     basic.showString("I L")
     basic.showIcon(IconNames.Heart)
     basic.showString("VE YOU!")
+    music.play(music.createSoundExpression(WaveShape.Sine, 5000, 500, 255, 0, 5000, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
 })
